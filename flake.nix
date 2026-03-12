ad7c33a3e1f14435447010e52018bda2e6e828ac
Ronny Esterluss <ronny.esterluss@hoprnet.org>
2026-03-12 23:34:40 +0100

    chore(deps): update to latest

--- flake.nix
+++ flake.nix
@@ -4,14 +4,32 @@
   inputs = {
     nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
     flake-utils.url = "github:numtide/flake-utils";
+
+    treefmt-nix = {
+      url = "github:numtide/treefmt-nix";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
   };
 
-  outputs = { self, nixpkgs, flake-utils }:
+  outputs = { self, nixpkgs, flake-utils, treefmt-nix }:
     flake-utils.lib.eachSystem [ "x86_64-linux" "aarch64-linux" "aarch64-darwin" "x86_64-darwin" ] (system:
       let
         pkgs = nixpkgs.legacyPackages.${system};
+
+        treefmtEval = treefmt-nix.lib.evalModule pkgs {
+          projectRootFile = "flake.nix";
+
+          programs.nixpkgs-fmt.enable = true;
+          programs.shfmt.enable = true;
+          programs.deno.enable = true;
+        };
       in
       {
+        formatter = treefmtEval.config.build.wrapper;
+        checks = {
+          formatting = treefmtEval.config.build.check self;
+        };
+
         devShells.default = pkgs.mkShell {
           buildInputs = with pkgs; [
             just
@@ -28,11 +46,10 @@
             curl
             openssl
           ];
-          
+
           shellHook = ''
             alias ll='ls -al'
           '';
-
         };
       }
     );
